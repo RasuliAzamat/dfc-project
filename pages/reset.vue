c96e3985d875463bce2eaee7ca2099ede2111a96
RasuliAzamat <rasuliazamat@gmail.com>
2022-09-16 03:00:29 +0300

    fix container margin-top bug

--- pages/reset.vue
+++ pages/reset.vue
@@ -1,31 +1,31 @@
 <template>
-  <section>
-    <div class="flex flex-col md:flex-row items-center justify-between">
-      <div class="w-full md:basis-1/2 lg:basis-3/5">
-        <IllustrationsSignIn />
-      </div>
-      <div class="w-full md:basis-1/2 lg:basis-2/5">
-        <form action="/reset" method="post">
-          <h2 class="mb-10">Сброс пароля</h2>
-          <div class="mb-5">
-            <label for="email">Введите свой email адрес для восстановления пароля</label>
-            <input type="email" name="email" id="email" placeholder="Email" v-model="emailValue" />
-            <small v-if="emailError">{{ emailError }}</small>
-          </div>
-          <div class="mb-5 text-right">
-            <NuxtLink to="/login" class="underline text-primary">Вернуться к входу</NuxtLink>
-          </div>
-          <UiButton class="w-full">Сбросить</UiButton>
-        </form>
-      </div>
-    </div>
-  </section>
+    <section>
+        <div class="flex flex-col md:flex-row items-center justify-between -mt-20 lg:-mt-40">">
+            <div class="w-full md:basis-1/2 lg:basis-3/5">
+                <IllustrationsSignIn />
+            </div>
+            <div class="w-full md:basis-1/2 lg:basis-2/5">
+                <form action="/reset" method="post">
+                    <h2 class="mb-10">Сброс пароля</h2>
+                    <div class="mb-5">
+                        <label for="email">Введите свой email адрес для восстановления пароля</label>
+                        <input type="email" name="email" id="email" placeholder="Email" v-model="emailValue" />
+                        <small v-if="emailError">{{ emailError }}</small>
+                    </div>
+                    <div class="mb-5 text-right">
+                        <NuxtLink to="/login" class="underline text-primary">Вернуться к входу</NuxtLink>
+                    </div>
+                    <UiButton class="w-full">Сбросить</UiButton>
+                </form>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup></script>
 
 <style lang="postcss" scoped>
 small {
-  @apply block text-red-500;
+    @apply block text-red-500;
 }
 </style>
---
+++ pages/signin.vue
@@ -0,0 +1,48 @@
+<template>
+    <section>
+        <div class="flex flex-col md:flex-row items-center justify-between -mt-20 lg:-mt-40">
+            <div class="w-full md:basis-1/2 lg:basis-3/5">
+                <IllustrationsSignIn />
+            </div>
+            <div class="w-full md:basis-1/2 lg:basis-2/5">
+                <form action="/login" method="post">
+                    <h2 class="mb-10">Вход</h2>
+                    <div class="mb-5">
+                        <label for="email">Введите email</label>
+                        <input type="email" name="email" id="email" placeholder="Email" v-model="emailValue" />
+                        <small v-if="emailError">{{ emailError }}</small>
+                    </div>
+                    <div class="mb-5">
+                        <label for="password">Введите пароль</label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="Пароль"
+                            v-model="passwordValue"
+                        />
+                        <small v-if="passwordError">{{ passwordError }}</small>
+                    </div>
+                    <div class="mb-5 text-right">
+                        <NuxtLink to="/reset" class="underline text-primary">Забыли пароль?</NuxtLink>
+                    </div>
+                    <UiButton class="w-full mb-10">Войти</UiButton>
+                    <div class="text-center">
+                        <p>
+                            У вас нет учетной записы?
+                            <NuxtLink to="/regist" class="underline text-primary">Регистрация</NuxtLink>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup></script>
+
+<style lang="postcss" scoped>
+small {
+    @apply block text-red-500;
+}
+</style>
